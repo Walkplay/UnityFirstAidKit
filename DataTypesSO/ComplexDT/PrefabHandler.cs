9201bcae93a85826631b5b487728143748044bbf
Walkplay <ivshar@ukr.net>
2019-05-08 10:34:28 +0300

    Data injection
    Prefab - data connector interface

---
+++ DataTypesSO/ComplexDT/PrefabHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PrefabHandler : MonoBehaviour
+{
+    public abstract void InjectData(IData data);
+}
