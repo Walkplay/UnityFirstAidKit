f203062f6f4b23e0cf4b6b00226075b1642ce3e9
Walkplay <ivshar@ukr.net>
2019-05-08 10:31:42 +0300

    More complex data types
    Scriptable object that sets to prefab

---
+++ DataTypesSO/ComplexDT/LevelSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level", menuName = "Game/Level")]
+public class LevelSO : ScriptableObject , IData
+{
+    [SerializeField] public string Name;
+    [SerializeField] public int BestScore;
+    [SerializeField] public List<WaveSO> Waves;
+    [SerializeField] public List<Vector3> SpawnPoints;
+   
+
+
+}
+
+
+
---
+++ DataTypesSO/ComplexDT/UnitIconSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+
+/// <summary>
+///     ! Icon represents prefab ! 
+/// 
+/// </summary>
+[CreateAssetMenu(fileName = "New Icon", menuName = "Game/UnitIcon")]
+public class UnitIconSO : ScriptableObject, IData
+{
+    [SerializeField] public string Name;
+    [SerializeField] public GameObject Prefab;
+    [SerializeField] public Sprite _sprite;
+    [SerializeField] public int CastPrice;
+    
+
+}
