249ce8e7dc46740eeaeb1449b2e4cff4791ea9fd
Walkplay <ivshar@ukr.net>
2019-05-08 10:47:17 +0300

    IContent impl

---
+++ DataTypesSO/ComplexDT/IconsSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New IconList", menuName = "DataTypes/Icon List")]
+public class IconsSO : IContent
+{
+    [SerializeField] public List<UnitIconSO> icons;
+
+    public override IData GetItem(int index)
+    {
+        return icons[index];
+    }
+
+    public override int GetListCount()
+    {
+        return icons.Count;
+    }
+}
---
+++ DataTypesSO/ComplexDT/LevelsSO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New LevelList", menuName = "DataTypes/Level List")]
+public class LevelsSO : IContent
+{
+    [SerializeField] public List<LevelSO> levels;
+
+    
+
+    public override IData GetItem(int index)
+    {
+        return levels[index];
+    }
+
+    public override int GetListCount()
+    {
+        return levels.Count;
+    }
+}
