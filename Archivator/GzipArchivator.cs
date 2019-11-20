8e1fba1dd8ccf35f481a8d6e3ed93d3754a04cc6
Walkplay <ivshar@ukr.net>
2019-11-20 03:34:34 +0200

    gzip/targz/zip archivator tool

---
+++ Archivator/GzipArchivator.cs
@@ -0,0 +1,50 @@
+﻿using PackageHandler.Interfaces;
+using System.IO.Compression;
+using System.IO;
+
+namespace PackageHandler.DecompressModule
+{
+    public class GzipArchivator : IArchivator
+    {
+        public string GetNestedFileName(string zipFilePath)
+        {
+            return string.Empty;
+        }
+
+        public void Decompress(string fileToDecompress, string destinationPath)
+        {
+            FileInfo fileInfo = new FileInfo(fileToDecompress);
+            using (FileStream originalFileStream = fileInfo.OpenRead())
+            {
+                using (FileStream decompressedFileStream = File.Create(destinationPath))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+
+        public void Compress(string fileToCompress, string destination = null)
+        {
+            FileInfo fileInfo = new FileInfo(fileToCompress);
+            using (FileStream originalFileStream = fileInfo.OpenRead())
+            {
+                if ((File.GetAttributes(fileInfo.FullName) &
+                    FileAttributes.Hidden) != FileAttributes.Hidden & fileInfo.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(fileInfo.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Archivator/ZipAchivator.cs
@@ -0,0 +1,42 @@
+﻿using PackageHandler.Interfaces;
+using Ionic.Zip;
+using System.Linq;
+
+namespace PackageHandler.DecompressModule
+{
+    public class ZipArchivator : IArchivator
+    {
+        public string GetNestedFileName(string zipFilePath)
+        {
+            using (ZipFile zip = ZipFile.Read(zipFilePath))
+            {
+                return zip.Entries.First().FileName;
+            }
+        }
+
+        public void Decompress(string fileToDecompress, string destinationPath)
+        {
+            using (ZipFile zip = ZipFile.Read(fileToDecompress))
+            {
+                foreach (ZipEntry e in zip)
+                {
+                    e.Extract(destinationPath, ExtractExistingFileAction.OverwriteSilently);  // overwrite existing files
+                }
+
+            }
+        }
+
+        public void Compress(string fileToCompress, string destinationPath = null)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                ZipEntry ze = zip.AddItem(fileToCompress);
+                if (destinationPath == null)
+                    zip.Save(fileToCompress + ".zip");
+                else
+                    zip.Save(destinationPath);
+            }
+        }
+
+    }
+}
